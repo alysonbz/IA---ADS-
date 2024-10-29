0e1d0a429f0b548d0c8aa4fa4463470bf94b8ab1
Eric Mesquita <ericmesqui2468@gmail.com>
2024-10-29 09:56:31 -0300

    pre-processing done

--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -3,21 +3,28 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Mostre a dimensão do dataset volunteer
-print(___)
+print("Print do tamanho do dataset")
+print(volunteer.shape)
 
 #mostre os tipos de dados existentes no dataset
-___
+print("Print das características do dataset")
+print(volunteer.info())
 
 #mostre quantos elementos do dataset estão faltando na coluna
----
+print("Elementos faltando na coluna locality")
+print(volunteer["locality"].isna().sum())
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_cols =
+print("Removendo Lat e Lon do dataset")
+volunteer_cols = volunteer.drop(["Latitude", "Longitude"], axis=1)
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_cols
-volunteer_subset =
+print("Removendo NaNs da col category_desc do dataset")
+volunteer_subset = volunteer_cols.dropna(subset="category_desc")
+print(volunteer_subset["category_desc"].isna().sum())
 
 # Print o shape do subset
-___
+print("shape do dataset volunteer_shape")
+print(volunteer_subset.shape)
 
 
