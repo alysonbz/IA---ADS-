e0871aa2c42b620ed83151cb89a19306a9f106b5
Esdras S <souzaesdras859@gmail.com>
2024-11-01 15:22:26 -0300

    Primeira Atividade Pronta

--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -3,21 +3,35 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Mostre a dimensão do dataset volunteer
-print(___)
+print("Dimensão")
+print(volunteer.shape)
+print("\n")
 
 #mostre os tipos de dados existentes no dataset
-___
+print("Tipos")
+print(volunteer.dtypes)
+print("\n")
 
 #mostre quantos elementos do dataset estão faltando na coluna
----
+print("Quantos estao faltando na coluna")
+print(volunteer.isna().sum())
+print("\n")
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_cols =
+print("Removendo Longitude e Latitude")
+volunteer_cols = volunteer.drop(columns="Longitude")
+volunteer_cols = volunteer_cols.drop(columns="Latitude")
+print(volunteer_cols.isna().sum())
+print("\n")
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_cols
-volunteer_subset =
+print("Removendo os Nulo do Categoty_desc")
+volunteer_subset = volunteer_cols.dropna(subset=["category_desc"])
+print(volunteer_subset.isna().sum())
+print("\n")
 
 # Print o shape do subset
-___
+print("Shape do subset")
+print(volunteer_subset.shape)
 
 
