efe085c822ba1e5cb2863b5811ece09096ef3c1f
Theus1990 <matheusrabelo9@hotmail.com>
2024-11-04 16:04:34 -0300

    Ex 5 and adjust

--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -3,14 +3,14 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Print os primeiros elementos da coluna hits
-print(volunteer.head())
+print(volunteer.shape)
 
 # Print as caracteristicas da coluna hits
-print(volunteer.info)
+print(volunteer["hits"].info())
 
 # Converta a coluna hits para o tipo int
 
-volunteer_new = volunteer.astype("int64")
+volunteer_new = volunteer["hits"].astype("int32")
 
 # Print as caracteristicas da coluna hits novamente
 print(volunteer_new.info)
--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -5,6 +5,10 @@ volunteer = load_volunteer_dataset()
 ## realize print do dataset volunteer corrgido sem nenhum NAN, removam as colunas NAN e depois as linhas e crie
 #um dataframe novo.
 
-print(volunteer)
+print(volunteer.info())
 
-volunteer_null = volunteer
+volunteerNaN = volunteer.drop(["is_priority", "amsl", "primary_loc","amsl_unit"], axis=1)
+
+volunteerNaN = volunteerNaN.dropna()
+
+print(volunteerNaN)
