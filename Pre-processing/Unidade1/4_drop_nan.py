6b17c527a494789c0ce703dbe61c5cd0b923022a
Eric Mesquita <ericmesqui2468@gmail.com>
2024-11-04 11:06:39 -0300

    data_nan done

--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -5,4 +5,10 @@ volunteer = load_volunteer_dataset()
 ## realize print do dataset volunteer corrgido sem nenhum NAN, removam as colunas NAN e depois as linhas e crie
 #um dataframe novo.
 
-print(volunteer)
+print(volunteer.info())
+
+volunteer_cols = volunteer.drop(['is_priority', 'amsl', 'amsl_unit', 'primary_loc'], axis=1)
+
+volunteer_final = volunteer_cols.dropna()
+
+print(volunteer_final.info())
