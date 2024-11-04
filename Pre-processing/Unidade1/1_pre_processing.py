f2fbea698003535aea9d5d49f198f839e64dfc71
theus1990 <matheusrabelo9@hotmail.com>
2024-11-04 10:13:10 -0300

    Ex1

--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -3,21 +3,19 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Mostre a dimensão do dataset volunteer
-print(___)
+print(volunteer.shape)
 
 #mostre os tipos de dados existentes no dataset
-___
-
+print(volunteer.type)
 #mostre quantos elementos do dataset estão faltando na coluna
----
-
+print(volunteer.isnull.sum())
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_cols =
+volunteer_cols = volunteer.drop([['Latitude', 'Longitude']])
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_cols
-volunteer_subset =
+volunteer_subset = volunteer.dropna([['category_desc', 'volunteer_cols']])
 
 # Print o shape do subset
-___
+print(volunteer_subset.shape)
 
 
