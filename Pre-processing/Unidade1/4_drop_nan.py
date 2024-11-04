69bedbbda0172a6b7f8fe0e3a778ba7feb2e8bec
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2024-11-04 10:50:57 -0300

    IA

---
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -0,0 +1,6 @@
+from src.utils import load_volunteer_dataset
+
+volunteer = load_volunteer_dataset()
+
+## realize print do dataset volunteer corrgido sem nenhum NAN, removam as colunas NAN e depois as linhas e crie
+#um dataframe novo.
