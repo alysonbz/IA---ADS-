411e3dcd1af502f1754c365771bdb48722a608c2
Robertmichaelavila <programadorwebpro9@gmail.com>
2024-11-04 22:02:34 -0300

    update feacture 2

--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -3,17 +3,13 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Print os primeiros elementos da coluna hits
-print(volunteer.hits[1])
-print(volunteer.hits[2])
-print(volunteer.hits[3])
-print(volunteer.hits[4])
-print(volunteer.hits[5])
+print(volunteer["hits"].head())
 
 # Print as caracteristicas da coluna hits
-print()
+print(volunteer["hits"].info())
 
 # Converta a coluna hits para o tipo int
-print()
+volunteer["hits"] = volunteer["hits"].astype("int32")
 
 # Print as caracteristicas da coluna hits novamente
-print()
+print(volunteer["hits"].info())
