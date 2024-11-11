49ecdd3f971ac7c43cd1bbf3dab8a7a6d4b79a42
1m0rr1v3l <lsxyz2017@gmail.com>
2024-11-11 09:02:32 -0300

    update activities

--- Pre-processing/Unidade1/3_training_and_test_sets.py
+++ Pre-processing/Unidade1/3_training_and_test_sets.py
@@ -1,25 +1,36 @@
 from src.utils import load_volunteer_dataset
-_____
+
 
 volunteer = load_volunteer_dataset()
 
+
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_new = __
+volunteer_new = volunteer.columns.drop("Latitude", "Longitude")
+
+print(volunteer_new)
+
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_new
-volunteer = ___
+volunteer = volunteer_new.dropna(subset=['category_desc'])
+
 
 # mostre o balanceamento das classes em 'category_desc'
-print(___['category_desc'].__,'\n','\n')
+print(volunteer_new['category_desc'].value_counts(),'\n','\n')
 
 # Crie um DataFrame com todas as colunas, com exceção de ``category_desc``
-X = volunteer.__(__, axis=1)
+X = volunteer_new.drop("category_desc", axis=1)
+
+print(X)
+
 
 # Crie um dataframe de labels com a coluna category_desc
-y = __[['__']]
+# y = __[['__']]
 
+'''
 # # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
 X_train, X_test, y_train, y_test = __(__, __, stratify=__, random_state=42)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
 ___
+
+'''
