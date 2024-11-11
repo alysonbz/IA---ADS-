ebdbaf68d705bac8f04b500d3cfd0106a3e7a72e
Eric Mesquita <ericmesqui2468@gmail.com>
2024-11-10 21:26:52 -0300

    manual train split test

--- Pre-processing/Unidade1/5_manual_trainnin_and_test_sets.py
+++ Pre-processing/Unidade1/5_manual_trainnin_and_test_sets.py
@@ -1,11 +1,25 @@
+import numpy as np
+import pandas as pd
 from src.utils import load_volunteer_dataset
 
 volunteer = load_volunteer_dataset()
+Y_COLUMN = "category_desc"
+def train_test_split(X, y, test_size=0.2, random_seed=1):
+    # Converte y para DataFrame, se necessário
+    if isinstance(y, np.ndarray):
+        y = pd.DataFrame(y, columns=[Y_COLUMN])
 
-def train_test_split(X,y,test_size,random_seed=1):
+    data = pd.concat([X, y], axis=1)
+    # Embaralha os dados
+    data = data.sample(frac=1, random_state=random_seed)
+    X = data.drop(Y_COLUMN, axis=1)
+    y = data[[Y_COLUMN]]  # Transforma y em uma série para que seja 1D
+    # Implementação manual do split
+    split_index = int(len(X) * (1 - test_size))
+    X_train, X_test = X[:split_index], X[split_index:]
+    y_train, y_test = y[:split_index], y[split_index:]
 
-    return X_train,X_test, y_train, y_test
-
+    return X_train, X_test, y_train, y_test
 
 # Exclua as colunas Latitude e Longitude de volunteer
 volunteer_new = volunteer.drop(["Latitude", "Longitude"], axis=1)
@@ -23,9 +37,9 @@ X = volunteer.drop("category_desc", axis=1)
 y = volunteer[['category_desc']]
 
 # # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-test_size = 2
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size,random_seed=1)
+test_size = 0.2
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size, random_seed=42)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
-print(y_test["category_desc"].value_counts())
-print(y_train["category_desc"].value_counts())
+print("Teste: ", y_test["category_desc"].value_counts())
+print("Treino: ", y_train["category_desc"].value_counts())
