950a73691e3dad1d72dc69570ed5884824e4b39f
theus1990 <matheusrabelo9@hotmail.com>
2024-11-18 08:38:23 -0300

    Unidade 2 e 3

---
+++ Pre-processing/Unidade2/Atividade_de_casa.py
@@ -0,0 +1,107 @@
+
+import math
+import pandas as pd
+
+def dist_euclidiana(v1, v2):
+    return math.sqrt(sum((v1[i] - v2[i]) ** 2 for i in range(len(v1) - 1)))
+
+def dist_manhattan(v1, v2):
+    return sum(abs(v1[i] - v2[i]) for i in range(len(v1) - 1))
+
+def dist_minkowski(v1, v2, p=3):
+    return sum(abs(v1[i] - v2[i]) ** p for i in range(len(v1) - 1)) ** (1 / p)
+
+def dist_chebyshev(v1, v2):
+    return max(abs(v1[i] - v2[i]) for i in range(len(v1) - 1))
+
+def calcular_distancia(v1, v2, tipo="euclidiana", p=3):
+    if tipo == "euclidiana":
+        return dist_euclidiana(v1, v2)
+    elif tipo == "manhattan":
+        return dist_manhattan(v1, v2)
+    elif tipo == "minkowski":
+        return dist_minkowski(v1, v2, p)
+    elif tipo == "chebyshev":
+        return dist_chebyshev(v1, v2)
+    else:
+        raise ValueError("Tipo de distância não suportado.")
+
+
+def knn(treinamento, nova_amostra, K, tipo_distancia="euclidiana", p=3):
+    dists = {}
+    for i in range(len(treinamento)):
+        d = calcular_distancia(treinamento[i], nova_amostra, tipo=tipo_distancia, p=p)
+        dists[i] = d
+
+    k_vizinhos = sorted(dists, key=dists.get)[:K]
+    qtd_setosa, qtd_versicolor, qtd_virginica = 0, 0, 0
+
+    for indice in k_vizinhos:
+        if treinamento[indice][-1] == 1.0:
+            qtd_setosa += 1
+        elif treinamento[indice][-1] == 2.0:
+            qtd_versicolor += 1
+        else:
+            qtd_virginica += 1
+
+    a = [qtd_setosa, qtd_versicolor, qtd_virginica]
+    return a.index(max(a)) + 1.0
+
+
+
+lista = []
+with open('../dataset/iris.data', 'r') as f:
+    for linha in f.readlines():
+        a = linha.replace('\n', '').split(',')
+        if a[4] == 'Iris-setosa':
+            a[4] = 1.0
+        elif a[4] == 'Iris-versicolor':
+            a[4] = 2.0
+        elif a[4] == 'Iris-virginica':
+            a[4] = 3.0
+
+        valor = [float(n) for n in a]
+        lista.append(valor)
+
+pd.DataFrame(lista).info()
+
+def countclasses(lista):
+    setosa, versicolor, virginica = 0, 0, 0
+    for i in range(len(lista)):
+        if lista[i][4] == 1.0:
+            setosa += 1
+        if lista[i][4] == 2.0:
+            versicolor += 1
+        if lista[i][4] == 3.0:
+            virginica += 1
+    return [setosa, versicolor, virginica]
+
+p = 0.6
+setosa, versicolor, virginica = countclasses(lista)
+treinamento, teste = [], []
+max_setosa, max_versicolor, max_virginica = int(p * setosa), int(p * versicolor), int(p * virginica)
+total1, total2, total3 = 0, 0, 0
+
+for lis in lista:
+    if lis[-1] == 1.0 and total1 < max_setosa:
+        treinamento.append(lis)
+        total1 += 1
+    elif lis[-1] == 2.0 and total2 < max_versicolor:
+        treinamento.append(lis)
+        total2 += 1
+    elif lis[-1] == 3.0 and total3 < max_virginica:
+        treinamento.append(lis)
+        total3 += 1
+    else:
+        teste.append(lis)
+
+acertos, K = 0, 1
+tipo_distancia = "euclidiana"  # "manhattan", "minkowski", "chebyshev"
+p_minkowski = 3  # Valor de 'p' para Minkowski
+
+for amostra in teste:
+    classe = knn(treinamento, amostra, K, tipo_distancia=tipo_distancia, p=p_minkowski)
+    if amostra[-1] == classe:
+        acertos += 1
+
+print(f"Porcentagem de acertos usando {tipo_distancia}: {100 * acertos / len(teste):.2f}%")
--- Supervised_Learning/unidade3/2_evaluate_knn.py
+++ Supervised_Learning/unidade3/2_evaluate_knn.py
@@ -1,20 +1,46 @@
-from sklearn.neighbors import KNeighborsClassifier
 from src.utils import load_churn_dataset
-
-# Import the module
-____
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
 
 churn_df = load_churn_dataset()
 X = churn_df[["account_length",  "total_day_charge" , "total_eve_charge",  "total_night_charge","total_intl_charge","number_customer_service_calls"]].values
 y = churn_df["churn"].values
 
 # Split into training and test sets
-X_train, X_test, y_train, y_test = ____(____, ____, test_size=____, random_state=____, stratify=____)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-knn = KNeighborsClassifier(n_neighbors=5)
+# Create neighbors
+neighbors = np.arange(1, 12)
+train_accuracies = {}
+test_accuracies = {}
 
-# Fit the classifier to the training data
-____
+for neighbor in neighbors:
+    # Set up a KNN Classifier
+    knn = KNeighborsClassifier(n_neighbors=neighbor)
 
-# Print the accuracy
-print(knn.score(____, ____))
+    # Fit the model
+    knn.fit(X_train,y_train)
+
+    # Compute accuracy
+    train_accuracies[neighbor] = knn.score(X_train, y_train)
+    test_accuracies[neighbor] = knn.score(X_test, y_test)
+
+print("acuracy on train: ",train_accuracies, '\n', "acuracy on test: ", test_accuracies)
+
+# Add a title
+plt.title("KNN: Verying Number of Neighbors.")
+
+# Plot training accuracies
+plt.plot(neighbors, train_accuracies.values(), label="Training Accuracy")
+
+# Plot test accuracies
+plt.plot(neighbors, test_accuracies.values(), label="Testing Accuracy")
+
+plt.legend()
+plt.xlabel("Number of Neighbors")
+plt.ylabel("Accuracy")
+
+# Display the plot
+plt.show()
