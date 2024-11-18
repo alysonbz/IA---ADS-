3db43f96c6259ee1b0c8b19efee8824f16d44eec
Davi Gomes <jose.davi@alu.ufc.br>
2024-11-18 13:24:46 -0300

    Atividades aula 18/11

--- Supervised_Learning/unidade4/1_create_features.py
+++ Supervised_Learning/unidade4/1_create_features.py
@@ -1,16 +1,18 @@
 import numpy as np
+from numpy.ma.core import reshape
+
 from src.utils import load_sales_clean_dataset
 
 sales_df = load_sales_clean_dataset()
 
 # Create X from the radio column's values
-X = ____
+X = sales_df.radio
 
 # Create y from the sales column's values
-y = ____
+y = sales_df.sales
 
 # Reshape X
-X = ____
+X = reshape(-1, 1)
 
 # Check the shape of the features and targets
-print(____)
+print(X, y)
