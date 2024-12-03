d4b4808dcfbbefcecab8ea5fc368755f529d123f
Esdras S <souzaesdras859@gmail.com>
2024-12-03 17:27:15 -0300

    Unidade 4 Feita

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -1,17 +1,22 @@
+import numpy
 import numpy as np
 from src.utils import processing_all_features_sales_clean
 
 def compute_RSS(predictions,y):
-    RSS = None
+    RSS = np.square(y-predictions)
     return RSS
 def compute_MSE(predictions,y):
-    MSE= None
+    RSS = compute_RSS(predictions, y)
+    MSE = np.divide(RSS, len(predictions))
     return MSE
 def compute_RMSE(predictions,y):
-    RMSE = None
+    MSE = compute_MSE(predictions, y)
+    RMSE = np.sqrt(MSE)
     return RMSE
 def compute_R_squared(predictions,y):
-    r_squared = None
+    var_pred = np.sum(np.square(y-np.mean(y)))
+    var_data = compute_RSS(predictions, y)
+    r_squared = np.divide(var_pred, var_data)
     return r_squared
 
 
