fbc84a51da53e4bfe768d7b49003786409a441d2
Luanvraft <luanbarroso1212@gmail.com>
2024-12-03 23:36:19 -0300

    questão 1 da AV1 atualizado

--- AV1/questao1.py
+++ AV1/questao1.py
@@ -8,3 +8,27 @@ waterQuality = load_water_quality()
 print(waterQuality.shape)
 print(waterQuality.isna().sum())
 print(waterQuality)
+
+'''
+aluminium - perigoso se for maior que 2.8
+ammonia - perigoso se for maior que 32.5
+arsenic - perigoso se for maior que 0.01
+barium - perigoso se for maior que 2
+cadmium - perigoso se for maior que 0.005
+chloramine - perigosa se for maior que 4
+chromium - perigoso se for maior que 0.1
+copper - perigoso se for maior que 1.3
+flouride - perigoso se for maior que 1.5
+bacteria - perigosas se forem maiores que 0
+viruses - perigoso se forem maiores que 0
+lead - perigoso se forem maiores que 0.015
+nitrates - perigoso se forem maiores que 10
+nitrites - perigoso se forem maiores que 1
+mercury - perigoso se forem maiores que 0.002
+perchlorate - perigoso se forem maiores que 56
+radium - perigoso se forem maiores que 5
+selenium - perigoso se forem maiores que 0.5
+silver - perigoso se forem maiores que 0.1
+uranium - perigoso se forem maiores que 0.3
+is_safe - atributo de classe {0 - não seguro, 1 - seguro}
+'''
--- AV1/questao2.py
+++ AV1/questao2.py
@@ -4,4 +4,22 @@ import numpy as np
 
 waterQuality = load_water_quality()
 
+def knn(treinamento_eu, nova_amostra, K):
+    dists, len_treino = {}, len(treinamento_eu)
 
+    for i in range(len_treino):
+        d = dist_euclidiana(treinamento_eu[i], nova_amostra)
+        dists[i] = d
+
+    k_vizinhos = sorted(dists, key=dists.get)[:K]
+
+    qtd_setosa, qtd_versicolor, qtd_virginica = 0, 0, 0
+    for indice in k_vizinhos:
+        if treinamento_eu[indice][-1] == 1.0:
+            qtd_setosa += 1
+        elif treinamento_eu[indice][-1] == 2.0:
+            qtd_versicolor += 1
+        else:
+            qtd_virginica += 1
+    a_eu = [qtd_setosa, qtd_versicolor, qtd_virginica]
+    return a_eu.index(max(a_eu)) + 1.0
