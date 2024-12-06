b87aaff3fc17c8883cd1edd7dfa4b00ab54ca1c5
Luanvraft <luanbarroso1212@gmail.com>
2024-12-06 04:16:14 -0300

    questão 5 da AV1 parcial

--- AV1/src/utils.py
+++ AV1/src/utils.py
@@ -3,9 +3,12 @@ import pandas as pd
 def load_water_quality_old():
     return pd.read_csv("dataset/water_quality.csv")
 
-def load_car_price_prediction():
-    return pd.read_csv("dataset/car_price_prediction.csv")
-
 def load_water_quality():
     return pd.read_csv("dataset/water_quality_ajustado.csv")
+
+def load_car_price_prediction_old():
+    return pd.read_csv("dataset/car_price_prediction.csv")
+
+def load_car_price_prediction():
+    return pd.read_csv("dataset/car_price_prediction_ajustado.csv")
 
