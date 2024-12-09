545bc83878220d963d2e8e4035e666293a301b0e
Rockisho <104766987+Rockisho@users.noreply.github.com>
2024-12-08 21:27:56 -0300

    Update

--- Supervised_Learning/unidade4/6_cross_validadation_r_2.py
+++ Supervised_Learning/unidade4/6_cross_validadation_r_2.py
@@ -1,28 +1,28 @@
 from src.utils import load_sales_clean_dataset
 from sklearn.linear_model import LinearRegression
-# Import the necessary modules
-from ____.____ import ____, ____
+from sklearn.model_selection import cross_val_score, KFold
 
+# Load the dataset
 sales_df = load_sales_clean_dataset()
+
 # Create X and y arrays
 X = sales_df["radio"].values.reshape(-1, 1)
 y = sales_df["sales"].values
 
+# Create a KFold object with k=6 and shuffling enabled
+kf = KFold(n_splits=6, shuffle=True, random_state=5)
 
-#Create a KFold object
-kf = ____(n_splits=____, shuffle=____, random_state=5)
-
+# Create the linear regression model
 reg = LinearRegression()
 
 # Compute 6-fold cross-validation scores
-cv_scores = ____(____, ____, ____, cv=____)
+cv_scores = cross_val_score(reg, X, y, cv=kf)
 
 # Print cv_scores
-print(____)
+print("Cross-validation scores:", cv_scores)
 
-# Print the mean
-print(___(__))
+# Print the mean of cv_scores
+print("Mean of cv_scores:", cv_scores.mean())
 
-# Print the standard deviation
-print(___(__))
-
+# Print the standard deviation of cv_scores
+print("Standard deviation of cv_scores:", cv_scores.std())
