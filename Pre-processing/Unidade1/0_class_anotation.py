d1a8f733f8e3088f556431e8f1c1573a7a2e7d51
Rockisho <104766987+Rockisho@users.noreply.github.com>
2024-12-08 21:15:39 -0300

    Update

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -1,10 +1,34 @@
-from src.utils import load_hiking_dataset , load_df2_unidade1,load_wine_dataset, load_df1_unidade1, load_volunteer_dataset
+from src.utils import load_hiking_dataset, load_df2_unidade1, load_wine_dataset, load_df1_unidade1, \
+    load_volunteer_dataset
 import pandas as pd
 
 volunteer = load_volunteer_dataset()
 hiking = load_hiking_dataset()
-wine  = load_wine_dataset()
+wine = load_wine_dataset()
 df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
+print(hiking.head())
+print(hiking.info())
+print(wine.describe())
 
+print("DF1")
+print(df1)
+print(df1.dropna())
+print("====================")
+print(df1.drop([1, 2, 3]))
+print("====================")
+print(df1.drop("A", axis=1))
+print("====================")
+print(df1.isna().sum())
+print("====================")
+print(df1.dropna(subset=["B"]))
+print("====================")
+print(df1.dropna(thresh=2))
+
+print("DF2")
+print(df2)
+print(df2.info())
+df2["C"] = df2["C"].astype("int64")
+
+print(df1.info())
