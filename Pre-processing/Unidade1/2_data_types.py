41fa0780a2db020174ef63b341a95c52d49b9e41
Rockisho <104766987+Rockisho@users.noreply.github.com>
2024-12-08 21:16:20 -0300

    Update

--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -2,14 +2,27 @@ from src.utils import load_volunteer_dataset
 
 volunteer = load_volunteer_dataset()
 
-# Print os primeiros elementos da coluna hits
-___
+# Mostre a dimensão do dataset volunteer
+print("Print do tamanho do dataset")
+print(volunteer.shape)
 
-# Print as caracteristicas da coluna hits
-__
+#mostre os tipos de dados existentes no dataset
+print("Print das características do dataset")
+print(volunteer.info())
 
-# Converta a coluna hits para o tipo int
-___
+#mostre quantos elementos do dataset estão faltando na coluna
+print("Elementos faltando na coluna locality")
+print(volunteer["locality"].isna().sum())
 
-# Print as caracteristicas da coluna hits novamente
----
+# Exclua as colunas Latitude e Longitude de volunteer
+print("Removendo Lat e Lon do dataset")
+volunteer_cols = volunteer.drop(["Latitude", "Longitude"], axis=1)
+
+# Exclua as linhas com valores null da coluna category_desc de volunteer_cols
+print("Removendo NaNs da col category_desc do dataset")
+volunteer_subset = volunteer_cols.dropna(subset="category_desc")
+print(volunteer_subset["category_desc"].isna().sum())
+
+# Print o shape do subset
+print("shape do dataset volunteer_shape")
+print(volunteer_subset.shape)
