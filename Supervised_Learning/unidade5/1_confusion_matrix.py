387d16ba0303571da4b624b830c0b285e0640dbb
Eric Mesquita <ericmesqui2468@gmail.com>
2025-01-20 02:32:16 -0300

    unit 5 lesson 1 fix & 2

--- Supervised_Learning/unidade5/1_confusion_matrix.py
+++ Supervised_Learning/unidade5/1_confusion_matrix.py
@@ -15,8 +15,7 @@ y = diabetes_df['diabetes'].values
 
 X_norm = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X_norm, y, stratify=y, random_state=42)
-
+X_train, X_test, y_train, y_test = train_test_split(X_norm, y, stratify=y, random_state=42, test_size=0.3)
 
 knn = KNeighborsClassifier(n_neighbors=7)
 
