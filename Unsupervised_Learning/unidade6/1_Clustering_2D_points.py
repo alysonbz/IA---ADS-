733c0be76d3c86ca66c534add7d751f513d5059d
Rockisho <104766987+Rockisho@users.noreply.github.com>
2025-01-21 07:22:07 -0500

    Add files via upload

--- Unsupervised_Learning/unidade6/1_Clustering_2D_points.py
+++ Unsupervised_Learning/unidade6/1_Clustering_2D_points.py
@@ -2,38 +2,40 @@ from src.utils import load_points
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
+# Carregar os pontos
 points = load_points()
 
-# Create a KMeans instance with 3 clusters: model
-model = __(__)
+# Criar uma instância do KMeans com 3 clusters
+model = KMeans(n_clusters=3)
 
-test_points = points[:50,:]
-train_points = points[50:,:]
+# Separar os pontos de teste e treino
+test_points = points[:50, :]  # primeiros 50 pontos
+train_points = points[50:, :]  # restantes
 
-# Fit model to train_points
-___
+# Ajustar o modelo aos pontos de treinamento
+model.fit(train_points)
 
-# Determine the cluster labels of new_points: labels
-labels = __
+# Prever os rótulos dos pontos de teste
+labels = model.predict(test_points)
 
-# Print cluster labels of new_points
+# Imprimir os rótulos dos clusters dos pontos de teste
 print(labels)
 
+# Atribuir as colunas de test_points às variáveis xs e ys
+xs = test_points[:, 0]
+ys = test_points[:, 1]
 
-# Assign the columns of test_points: xs and ys
-xs = test_points[:,0]
-ys = test_points[:,1]
+# Criar um scatter plot dos pontos de teste com as cores baseadas nos rótulos
+plt.scatter(xs, ys, c=labels, alpha=0.5)
 
-# Make a scatter plot of xs and ys, using labels to define the colors
-___
+# Obter os centróides dos clusters
+centroids = model.cluster_centers_
 
-# Assign the cluster centers: centroids
-centroids =
+# Atribuir as colunas dos centróides às variáveis centroids_x e centroids_y
+centroids_x = centroids[:, 0]
+centroids_y = centroids[:, 1]
 
-# Assign the columns of centroids: centroids_x, centroids_y
-centroids_x = centroids[:,0]
-centroids_y = centroids[:,1]
+# Fazer um scatter plot dos centróides
+plt.scatter(centroids_x, centroids_y, s=50, marker='D', color='red')
 
-# Make a scatter plot of centroids_x and centroids_y
-plt.scatter(centroids_x,centroids_y,s=50, marker = 'D')
 plt.show()
