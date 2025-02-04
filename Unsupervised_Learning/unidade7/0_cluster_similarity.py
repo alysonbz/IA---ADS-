852d834b68d40cf7ad79dbeab039cc2e6a35eb48
Luanvraft <luanbarroso1212@gmail.com>
2025-02-04 09:35:59 -0300

    unidade 7 nada funciona

---
+++ Unsupervised_Learning/unidade7/0_cluster_similarity.py
@@ -0,0 +1,29 @@
+import numpy as np
+import math
+
+
+def compute_single_linkage(cluster1,cluster2):
+    soma = 0
+    lista = []
+    for i in range(len(cluster1)):
+        for j in range(len(cluster1[i])):
+            soma += math.pow(cluster1[i][j] - cluster2[i][j], 2)
+            lista.append(math.sqrt(soma))
+    return np.min(lista)
+
+def compute_complete_linkage(cluster1, cluster2):
+    return None
+
+def compute_average_linkage(cluster1, cluster2):
+    return None
+
+def compute_centroid_linkage(cluster1,cluster2):
+    return None
+
+cluster1 = [[9.0,8.0],[6.0,4.0],[2.0,10.0],[3.0,6.0],[1.0,0.0]]
+cluster2 = [[7.0,4.0],[1.0,10.0],[6.0,10.0],[1.0,6.0],[7.0,1.0]]
+
+print("similaridade ligação simples: ", compute_single_linkage(cluster1,cluster2))
+print("similaridade ligação completa: ", compute_complete_linkage(cluster1,cluster2))
+print("similaridade ligação média: ", compute_average_linkage(cluster1,cluster2))
+print("similaridade pelo método do centroide: ", compute_centroid_linkage(cluster1,cluster2))
