ff9275c6d0198a3be6f6f85c443625eadad5465b
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2025-02-04 08:45:18 -0300

    aprendizado não supervisionado

--- Unsupervised_Learning/unidade7/0_cluster_similarity.py
+++ Unsupervised_Learning/unidade7/0_cluster_similarity.py
@@ -13,9 +13,6 @@ def compute_average_linkage(cluster1, cluster2):
 def compute_centroid_linkage(cluster1,cluster2):
     return None
 
-def compute_ward_linkage(cluster1,cluster2):
-    return None
-
 
 cluster1 = [[9.0,8.0],[6.0,4.0],[2.0,10.0],[3.0,6.0],[1.0,0.0]]
 cluster2 = [[7.0,4.0],[1.0,10.0],[6.0,10.0],[1.0,6.0],[7.0,1.0]]
@@ -24,7 +21,6 @@ print("similaridade ligação simples: ", compute_single_linkage(cluster1,cluste
 print("similaridade ligação completa: ", compute_complete_linkage(cluster1,cluster2))
 print("similaridade ligação média: ", compute_average_linkage(cluster1,cluster2))
 print("similaridade pelo método do centroide: ", compute_centroid_linkage(cluster1,cluster2))
-print("similaridade ligação simples: ", compute_ward_linkage(cluster1,cluster2))
 
 
 
