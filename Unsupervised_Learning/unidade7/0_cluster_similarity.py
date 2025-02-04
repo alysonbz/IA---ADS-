3eb14a01d056eed6e31aa474b6757f9b770e8be8
theus1990 <matheusrabelo9@hotmail.com>
2025-02-04 09:36:44 -0300

    Cluster Similarity

--- Unsupervised_Learning/unidade7/0_cluster_similarity.py
+++ Unsupervised_Learning/unidade7/0_cluster_similarity.py
@@ -1,17 +1,39 @@
 import numpy as np
+from scipy.cluster.hierarchy import centroid
 
 
 def compute_single_linkage(cluster1,cluster2):
-     return None
+    distMin = np.inf
+    for p1 in cluster1:
+        for p2 in cluster2:
+            distancia = np.linalg.norm(np.array(p1)-np.array(p2))
+            if distancia < distMin:
+                distMin = distancia
+
+    return distMin
 
 def compute_complete_linkage(cluster1, cluster2):
-     return None
+    distMax = 0
+    for p1 in cluster1:
+        for p2 in cluster2:
+            distancia = np.linalg.norm(np.array(p1) - np.array(p2))
+            if distancia > distMax:
+                distMax = distancia
+
+    return distMax
 
 def compute_average_linkage(cluster1, cluster2):
-     return None
+    distancia = 0
+    for p1 in cluster1:
+        for p2 in cluster2:
+            distancia += np.linalg.norm(np.array(p1) - np.array(p2))
+
+    return distancia/(len(cluster1)*len(cluster2))
 
 def compute_centroid_linkage(cluster1,cluster2):
-    return None
+    centroid1 = np.mean(cluster1, axis=0)
+    centroid2 = np.mean(cluster2, axis=0)
+    return np.linalg.norm(centroid1-centroid2)
 
 
 cluster1 = [[9.0,8.0],[6.0,4.0],[2.0,10.0],[3.0,6.0],[1.0,0.0]]
